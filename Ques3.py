550b5c2b4dc7b87e7dc6ca8364f22367dd58e6e3
Dheeraj-Batra <96729888+Dheeraj-Batra@users.noreply.github.com>
2022-01-06 20:12:54 +0530

    Delete Ques3.py

--- Ques3.py
+++
@@ -1,7 +0,0 @@
-name=input("enter your name= ")
-SID=int(input("enter your sid= "))
-cgpa=float(input("enter your cgpa= "))
-course=input("enter your course= ")
-gender=input("enter your gender= ")   #use the abbreviation M=male,F=female and U=unknown
-STUDENT=[name,SID,cgpa,course,gender]
-print(STUDENT)
