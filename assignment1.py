d44a464f2606c1688c62d62c67de746ee594359c
Dheeraj-Batra <96729888+Dheeraj-Batra@users.noreply.github.com>
2022-01-06 20:53:17 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,60 @@
+"""
+print("enter 1st num= ")
+x1=int(input())
+print("enter 2nd num= ")
+x2=int(input())
+print("enter 3rd num= ")
+x3=int(input())
+print((x1+x2+x3)/3)
+"""
+
+
+
+
+"""
+print("enter your gross income= ")
+gross_income=int(input())
+print("number of dependents= ")
+dependent=int(input())
+taxable_income=gross_income-10000-(dependent*3000)
+#taxable income = Gross Income - Standard deduction - (Dependent deduction * No. of dependents)
+print(taxable_income*0.2)    #tax to be paid = taxable income*tax rate
+"""
+
+
+
+
+"""
+name=input("enter your name= ")
+SID=int(input("enter your sid= "))
+cgpa=float(input("enter your cgpa= "))
+course=input("enter your course= ")
+gender=input("enter your gender= ")   #use the abbreviation M=male,F=female and U=unknown
+STUDENT=[name,SID,cgpa,course,gender]
+print(STUDENT)
+"""
+
+
+
+
+"""
+#studens have recieved marks out of 100
+my_list=["78","95","68","98","85"]
+my_list.sort()
+print(my_list)
+"""
+
+
+
+
+"""
+COLOUR_LIST=["Red","Green","White","Black","Pink","Yellow"]    #original list
+print(COLOUR_LIST)
+
+del COLOUR_LIST[3]
+print(COLOUR_LIST)      #list obtained after deleting colour black
+
+COLOUR_LIST=["Red","Green","White","Black","Pink","Yellow"]
+COLOUR_LIST[3]=COLOUR_LIST[4]="Purple"
+print(COLOUR_LIST)       #list obtained after replacing 4th and 5th element with pink colour
+"""
---
+++ assignment1_output.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\dheer\Documents\assignment1.py ===============
+enter 1st num= 
+10
+enter 2nd num= 
+20
+enter 3rd num= 
+30
+20.0
+
+=============== RESTART: C:\Users\dheer\Documents\assignment1.py ===============
+enter your gross income= 
+100000
+number of dependents= 
+3
+16200.0
+
+=============== RESTART: C:\Users\dheer\Documents\assignment1.py ===============
+enter your name= dheeraj
+enter your sid= 21104030
+enter your cgpa= 8.8
+enter your course= EE
+enter your gender= M
+['dheeraj', 21104030, 8.8, 'EE', 'M']
+
+=============== RESTART: C:\Users\dheer\Documents\assignment1.py ===============
+['68', '78', '85', '95', '98']
+
+=============== RESTART: C:\Users\dheer\Documents\assignment1.py ===============
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
