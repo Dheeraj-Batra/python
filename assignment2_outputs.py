eb35c295fd2e59389e1553205cb324c65bc7644d
Dheeraj-Batra <96729888+Dheeraj-Batra@users.noreply.github.com>
2022-01-23 00:09:12 +0530

    Add files via upload

---
+++ assignment2_outputs.py
@@ -0,0 +1,41 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\dheer\Desktop\PEC\ESC1  intro to COMP\assinments\assignment2.py
+original string is=  Python is a case sensitive language
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+
+= RESTART: C:\Users\dheer\Desktop\PEC\ESC1  intro to COMP\assinments\assignment2.py
+Hey, Dheeraj Batra Here!
+My sid is 21104030 
+I am from Electrical Engineering department and my cgpa is  9.9
+
+= RESTART: C:\Users\dheer\Desktop\PEC\ESC1  intro to COMP\assinments\assignment2.py
+8
+58
+50
+224
+40
+14
+0
+
+= RESTART: C:\Users\dheer\Desktop\PEC\ESC1  intro to COMP\assinments\assignment2.py
+enter 1st number= 8.8
+enter 2nd number= 15.0
+enter 3rd number= 22.6
+max number is= 22.6
+
+= RESTART: C:\Users\dheer\Desktop\PEC\ESC1  intro to COMP\assinments\assignment2.py
+enter the string= my name dheeraj 
+yes
+
+= RESTART: C:\Users\dheer\Desktop\PEC\ESC1  intro to COMP\assinments\assignment2.py
+enter 1st side= 1
+enter 2nd side= 2
+enter 3rd side= 3
+YES triangle is possible
