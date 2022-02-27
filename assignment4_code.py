6b7b46cde11b3985e5c71f41ffb5f422d85503a2
Dheeraj-Batra <96729888+Dheeraj-Batra@users.noreply.github.com>
2022-02-27 05:35:07 +0530

    Add files via upload

---
+++ assignment4_code.py
@@ -0,0 +1,214 @@
+print("QUESTION 1")
+
+# here n refers to number of discs used 
+#source refers to rod 1 , intermediate refers to rod 2 and destination refers to rod 3
+
+
+def TowerofHanoi(n, source, destination, intermediate):
+    if n == 1:     # base case 
+        print("move disk 1 from source", source, "to destination", destination)
+        return
+    TowerofHanoi(n-1, source, intermediate, destination)
+    print("move disk", n, "from source", source, "to destination", destination)
+    TowerofHanoi(n-1, intermediate, destination, source)
+
+n = 3
+TowerofHanoi(n, "Rod_1", "Rod_2", "Rod_3")
+
+print("\n")
+print("-"*50)
+
+print("QUESTION 2")
+
+
+def factorial(n):    # function for factorial
+    value=1
+    for i in range(1,n+1):
+        value=value*i
+    return value
+
+
+# # dont enter negative number otherwise code will exit
+n=int(input("enter the number of rows = "))
+if n<=0:
+    exit()
+else:
+    print("using iterative method")                              ## iterative method
+    for row in range(n):
+        for col in range(n-row+1):
+            print(end=" ")
+        for col in range(row+1):
+            combinatorics=(factorial(row)//(factorial(row-col)*factorial(col)))   # this represents nCr
+            print(combinatorics,end=" ")
+        print("")
+print("\n")
+
+print("using recusrive method")                                 ## recursion method
+def pascals_Triangle(n):
+        if n == 1:
+            return [[1]]
+        else:
+            answer=pascals_Triangle(n-1)
+            new_row = [1]
+            last_row=answer[-1]
+            for i in range(len(last_row)-1):
+                new_row.append(last_row[i] + last_row[i+1])
+            new_row += [1]
+            answer.append(new_row)
+        return answer
+
+triangle=pascals_Triangle(n)
+for i in triangle:
+        print(i)   
+print("\n")
+print("-"*50)
+
+
+
+
+print("QUESTION 3")
+
+first_num=int(input("enter the divident="))
+second_num=int(input("enter the divisor="))
+quotient=first_num//second_num
+remainder=first_num%second_num
+print(f"the quotient is {quotient} and remainder is {remainder}")
+print("\n")
+
+#part_a
+print("part_a=")
+a=print("value of first_num is",callable(quotient))
+b=print("value of second_num is",callable(remainder))
+c=print("value of quotient is",callable(first_num))
+d=print("value of remainder is",callable(second_num))
+print("\n")
+
+
+#parT_b
+print("part_b=")
+if (quotient==0 and remainder!=0):
+    print("only the quotient is zero")
+if (remainder==0 and quotient!=0):
+    print("only the remainder is zero")
+if(quotient!=0 and remainder!=0):
+    print("both the quotient and remainder are not zero")
+if(quotient==0 and remainder==0):
+    print("both the quotient and remainder are zero")
+print("\n")
+
+#part_c
+print("part_c=")
+list=list()
+list.append(quotient)
+list.append(remainder)
+list.append(4)
+list.append(5)
+list.append(6)
+new_list=[]
+for i in list:
+    if i>4:
+        new_list.append(i)
+print("new list is=",new_list)
+print("\n")
+
+#part_d
+print("part_d=")
+set=set(new_list)
+print("the set is=",set)
+print("\n")
+
+#part_e
+print("part_e=")
+print("immutable set is=",frozenset(set))
+print("\n")
+
+#part_f
+print("part_f=")
+print("max value is=",max(set))
+print("hash value is=",hash(max(set)))
+print("\n")
+print("-"*50)
+
+
+
+
+
+print("QUESTION 4")
+
+class Student():
+
+    def __init__(self, name, sid):
+        self.name = name
+        self.sid = sid
+
+    def details(self):
+        return(f"name of child is {self.name} and roll no is {self.sid}")
+
+    def __del__(self):
+        print("details of the object have been deleted successfully")
+
+    
+Dheeraj = Student("Dheeraj", 21104030)
+print(Dheeraj.details())
+
+del Dheeraj
+print("\n")
+print("-"*50)
+
+
+print("QUESTION 5")
+
+class Employee_Details():
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def details(self):
+        return(f"name of employe is {self.name} and his/her salary is {self.salary}")
+
+Mehak = Employee_Details("Mehak", 40000)
+Ashok = Employee_Details("Ashok", 50000)
+Viren = Employee_Details("Viren", 60000)
+print(Mehak.details())
+print(Ashok.details())
+print(Viren.details())
+print("\n")
+
+# part A
+Mehak.salary = 70000
+print("congo mehak ur salary has been updated to= ", Mehak.salary)
+print("\n")
+
+# part B
+del Viren
+print("details of viren have been deleted successfully")
+print("\n")
+print("-"*50)
+
+
+
+
+print("QUESTION 6")
+
+george_word=input("george please enter a word=").lower()
+
+#concept of anagrams has been used here 
+def anagrams(word):
+    if word=="":
+        return [word]
+    else:
+        ans=[]
+        for char in anagrams(word[1:]):
+            for pos in range(len(char)+1):
+                ans.append(char[:pos]+word[0]+char[pos:])
+        return ans
+possible_combination=anagrams(george_word)       
+# possible_combination variable stores all the anagrams possible
+
+barbie_word=input("barbie please enter a word=").lower()
+if barbie_word in possible_combination:
+    print("the friendship is real")
+else:
+    print("the friendship is not real")
+print("-"*50)
---
+++ assignment4_outputs.py
@@ -0,0 +1,93 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:\Users\dheer\Desktop\assignment4_code.py =============
+QUESTION 1
+move disk 1 from source Rod_1 to destination Rod_2
+move disk 2 from source Rod_1 to destination Rod_3
+move disk 1 from source Rod_2 to destination Rod_3
+move disk 3 from source Rod_1 to destination Rod_2
+move disk 1 from source Rod_3 to destination Rod_1
+move disk 2 from source Rod_3 to destination Rod_2
+move disk 1 from source Rod_1 to destination Rod_2
+
+
+--------------------------------------------------
+QUESTION 2
+enter the number of rows = 5
+using iterative method
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+
+using recusrive method
+[1]
+[1, 1]
+[1, 2, 1]
+[1, 3, 3, 1]
+[1, 4, 6, 4, 1]
+
+
+--------------------------------------------------
+QUESTION 3
+enter the divident=70
+enter the divisor=7
+the quotient is 10 and remainder is 0
+
+
+part_a=
+value of first_num is False
+value of second_num is False
+value of quotient is False
+value of remainder is False
+
+
+part_b=
+only the remainder is zero
+
+
+part_c=
+new list is= [10, 5, 6]
+
+
+part_d=
+the set is= {10, 5, 6}
+
+
+part_e=
+immutable set is= frozenset({10, 5, 6})
+
+
+part_f=
+max value is= 10
+hash value is= 10
+
+
+--------------------------------------------------
+QUESTION 4
+name of child is Dheeraj and roll no is 21104030
+details of the object have been deleted successfully
+
+
+--------------------------------------------------
+QUESTION 5
+name of employe is Mehak and his/her salary is 40000
+name of employe is Ashok and his/her salary is 50000
+name of employe is Viren and his/her salary is 60000
+
+
+congo mehak ur salary has been updated to=  70000
+
+
+details of viren have been deleted successfully
+
+
+--------------------------------------------------
+QUESTION 6
+george please enter a word=love
+barbie please enter a word=ovel
+the friendship is real
+--------------------------------------------------
